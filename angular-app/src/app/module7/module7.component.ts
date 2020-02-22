6b9d94e64ef77309801e902381c9884ac28a6ed2
Piotr Luczak <piotr.andrzej.luczak@gmail.com>
2020-02-22 21:35:11 +0000

    Pretty plotting

--- angular-app/src/app/module7/module7.component.ts
+++ angular-app/src/app/module7/module7.component.ts
@@ -44,7 +44,11 @@ export class Module7Component implements OnInit {
     return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
   }
 
-  static randomArray(min: number, max: number, len: number): number[] {
+  static gaussian(factor: number = 6) {
+    return Array.from({ length: factor }, () => Math.random()).reduce((accumulator, currentValue) => accumulator + currentValue) / factor;
+  }
+
+  static randomArray(len: number, min: number = 0, max: number = 1): number[] {
     return Array.from({ length: len }, () => this.random(min, max));
   }
 
@@ -52,35 +56,88 @@ export class Module7Component implements OnInit {
     return Array.from({ length: len }, () => this.normal());
   }
 
+  static gaussianArray(len: number, factor: number = 6) {
+    return Array.from({ length: len }, () => this.gaussian(factor));
+  }
+
   drawDistributions() {
     const trace1 = {
       x: this.distribution1.map((val, idx) => idx),
       y: this.distribution1,
+      type: 'scatter',
       mode: 'lines',
-      name: 'Distribution A'
+      name: 'Próba A',
+      line: {
+        color: '#1f77b4',
+        width: 2
+      }
     };
     const trace2 = {
       x: this.distribution2.map((val, idx) => idx),
       y: this.distribution2,
+      type: 'scatter',
       mode: 'lines',
-      name: 'Distribution B'
+      name: 'Próba B',
+      line: {
+        color: '#ff7f0e',
+        width: 2
+      }
     };
-    const data = [trace1, trace2];
+    const trace1h = {
+      x: this.distribution1,
+      xaxis: 'x2',
+      yaxis: 'y2',
+      type: 'histogram',
+      name: 'Próba A',
+      opacity: 0.75,
+      marker: {
+        color: '#1f77b4',
+        width: 1
+      },
+      xbins: {
+        end: 1,
+        size: 0.2,
+        start: 0
+      },
+      showlegend: false
+    };
+    const trace2h = {
+      x: this.distribution2,
+      xaxis: 'x2',
+      yaxis: 'y2',
+      type: 'histogram',
+      name: 'Próba B',
+      opacity: 0.75,
+      marker: {
+        color: '#ff7f0e',
+        width: 1
+      },
+      xbins: {
+        end: 1 * this.multiplier,
+        size: 0.2 * this.multiplier,
+        start: 0
+      },
+      showlegend: false
+    };
+    const data = [trace1, trace2, trace1h, trace2h];
     const layout = {
-      title: 'A & B distributions',
+      title: 'Próby A & B',
       plot_bgcolor: '#e9ecef',
-      paper_bgcolor:"#e9ecef"
+      paper_bgcolor: '#e9ecef',
+      grid: { rows: 1, columns: 2, pattern: 'independent' },
+      legend: { orientation: 'h', xanchor: 'center', x: 0.5, y: 1.1 }
     };
-    const config = {responsive: true}
+    const config = { responsive: true, staticPlot: false, displayModeBar: false };
     Plotly.newPlot(document.getElementById('plotlyChart'), data, layout, config);
   }
 
   ngOnInit() {
-    this.distribution1 = Module7Component.normalArray(1000).sort();
+    const arrLen = 100;
+    this.distribution1 = Module7Component.normalArray(arrLen);
     this.mean1 = Module7Component.mean(this.distribution1);
     this.stddev1 = Module7Component.stddev(this.distribution1);
     this.variation1 = Module7Component.variation(this.distribution1);
-    this.distribution2 = Module7Component.normalArray(1000).sort();
+    this.distribution2 = Module7Component.normalArray(arrLen);
     this.distribution2 = this.distribution2.map(val => val * this.multiplier);
     this.mean2 = Module7Component.mean(this.distribution2);
     this.stddev2 = Module7Component.stddev(this.distribution2);
