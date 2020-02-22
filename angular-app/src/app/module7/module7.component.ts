4a85e740554ed2b94d3d92cd05f8746ffdc1b158
Piotr Luczak <piotr.andrzej.luczak@gmail.com>
2020-02-22 08:36:50 +0000

    Initial stuff

--- angular-app/src/app/module7/module7.component.ts
+++ angular-app/src/app/module7/module7.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-module7',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./module7.component.scss']
 })
 export class Module7Component implements OnInit {
+  static mean(a: number[]): number {
+    return a.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / a.length;
+  }
+
+  static stddev(a: number[]): number {
+    const mean: number = this.mean(a);
+    const result: number = a.reduce((accumulator, currentValue) => accumulator + (currentValue - mean) ** 2, 0);
+    return Math.sqrt(result / a.length);
+  }
+
+  static variation(a: number[]): number {
+    return this.stddev(a) / this.mean(a);
+  }
 
   constructor() { }
 
