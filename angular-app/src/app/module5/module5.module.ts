40c38ba955e4699171d0f34731fc35d1175a03fc
Piotr Jedyk <jedykp@gmail.com>
2020-02-25 20:48:54 +0100

    module5: some theory

--- angular-app/src/app/module5/module5.module.ts
+++ angular-app/src/app/module5/module5.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { Module5RoutingModule } from './module5-routing.module';
 import { Module5Component } from './module5.component';
 
+import {MathJaxModule} from 'ngx-mathjax';
 
 @NgModule({
   declarations: [Module5Component],
   imports: [
     CommonModule,
-    Module5RoutingModule
+    Module5RoutingModule,
+    MathJaxModule.forChild()
   ]
 })
 export class Module5Module { }
