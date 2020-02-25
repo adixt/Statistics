703b4900c32011abef34300978808057d9117f70
Piotr Jedyk <jedykp@gmail.com>
2020-02-25 20:48:33 +0100

    added: ngx-mathjax

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { WelcomeComponent } from './welcome/welcome.component';
 
+import {MathJaxModule} from 'ngx-mathjax';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +17,11 @@ import { WelcomeComponent } from './welcome/welcome.component';
     NgbModule,
     BrowserModule,
     AppRoutingModule,
+    MathJaxModule.forRoot({
+      version: '2.7.5',
+      config: 'TeX-AMS_HTML',
+      hostname: 'cdnjs.cloudflare.com'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
